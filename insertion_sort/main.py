a081562adc48f85cd7b4f87337891b67f9723594
npcoiner <nicholascoiner@gmail.com>
2025-07-14 12:49:21 -0700

    Flipped insertion sort for understanding

--- insertion_sort/main.py
+++ insertion_sort/main.py
@@ -8,3 +8,17 @@ def insertion_sort(a: list[int]) -> list:
         a[i+1] = key
     return a
 
+def insertion_sort_flipped(a: list[int]) -> list:
+    j = len(a) - 2
+    while j >= 0:
+        key = a[j]
+        i = j + 1
+        while(i < len(a) and a[i] < key):
+            a[i-1] = a[i]
+            i = i + 1
+        a[i-1] = key
+        j = j - 1
+    return a
+
+
+        
--- insertion_sort/tests/test_insertion_sort.py
+++ insertion_sort/tests/test_insertion_sort.py
@@ -1,4 +1,6 @@
 from insertion_sort.main import insertion_sort
+from insertion_sort.main import insertion_sort_flipped
+
 import random
 
 def test_small():
@@ -10,5 +12,15 @@ def test_empty():
 def test_random():
     data = random.sample(range(1000), 100)
     assert insertion_sort(data) == sorted(data)
+
+def test_small_flipped():
+    assert insertion_sort_flipped([3,1,2]) == [1,2,3]
+
+def test_empty_flipped():
+    assert insertion_sort_flipped([]) == []
+
+def test_random_flipped():
+    data = random.sample(range(1000), 100)
+    assert insertion_sort_flipped(data) == sorted(data)
 
 
